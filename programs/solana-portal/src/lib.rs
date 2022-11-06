79403611e90b6b7db9d51934e7e6e0827f0bc940
Kevin <kevinfiorentino.1294@gmail.com>
2022-11-06 16:07:09 -0300

    Solana Counter

--- programs/solana-portal/src/lib.rs
+++ programs/solana-portal/src/lib.rs
@@ -1,16 +1,39 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod solana_portal {
     use super::*;
+    pub fn create_counter(ctx: Context<Counter>) -> Result<()> {
+        let base_counter = &mut ctx.accounts.base_counter;
+        base_counter.total = 0;
+        Ok(())
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn counter_add(ctx: Context<CounterAdd>) -> Result<()> {
+        let base_counter = &mut ctx.accounts.base_counter;
+        base_counter.total += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Counter<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_counter: Account<'info, BaseCounter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CounterAdd<'info> {
+    #[account(mut)]
+    pub base_counter: Account<'info, BaseCounter>,
+}
+
+#[account]
+pub struct BaseCounter {
+    pub total: u64,
+}
--- tests/solana-portal.js
+++ tests/solana-portal.js
@@ -1,13 +1,35 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
-  const program = anchor.workspace.SolanaPortal;
-  const tx = await program.rpc.initialize();
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
+  const program = anchor.workspace.SolanaPortal;
+  const baseCounter = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.createCounter({
+    accounts: {
+      baseCounter: baseCounter.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseCounter],
+  });
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseCounter.fetch(baseCounter.publicKey);
+  console.log('👀 GIF Count', account.total.toString())
+	
+  await program.rpc.counterAdd({
+    accounts: {
+      baseCounter: baseCounter.publicKey,
+    },
+  });
+  
+  account = await program.account.baseCounter.fetch(baseCounter.publicKey);
+  console.log('👀 GIF Count', account.total.toString())
 }
 
 const runMain = async () => {
